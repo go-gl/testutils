548b367dd8a1b7aaf310c6e3f0492d883f05db5c
Peter Waller <peter@pwaller.net>
2012-10-16 17:02:24 +0100

    Travis helper: install libsdl-image1.2-dev

--- travis-helper-functions.sh
+++ travis-helper-functions.sh
@@ -117,7 +117,8 @@ at "Installing packages"
 # it was significant (when installing the whole of lightdm for example), but
 # I'm not sure if it's the case here. It doesn't cost anything, so here it is.
 erl sudo eatmydata apt-get install -qq \
-  libglfw-dev libglew-dev mesa-utils inotify-tools xserver-xorg libsdl-dev
+  libglfw-dev libglew-dev mesa-utils inotify-tools xserver-xorg \
+  libsdl1.2-dev libsdl-image1.2-dev
 
 at "Starting X"
 erl sudo mkdir -p /tmp/.X11-unix
